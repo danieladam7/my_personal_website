ac4aca114d7a02d78bc36edaed085c53987990f1
Daniel Adam <danielyosef@gmail.com>
2024-06-15 16:16:53 +0300

    unit tests for class Tag in models

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -1,4 +1,14 @@
 from django.test import TestCase
-from .models import *
+from ..models import *
 
-class 
+
+class TagModelTest(TestCase):
+
+    def setUp(self):
+        self.tag = Tag.object.create(captio='Test-tag')
+
+    def test_tag_creation(self):
+        self.assertEqual(self.tag.caption, "Test-tag")
+
+    def test_tag_str(self):
+        self.assertEqual(str(self.tag), "Test-tag")
