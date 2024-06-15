4f28967a15b5a3cabc566e00afdad220c1169bb5
Daniel Adam <danielyosef@gmail.com>
2024-06-15 19:13:37 +0300

    implemented Skills View Test

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -13,3 +13,13 @@ class ContactViewTest(TestCase):
     def test_contact_view_template(self):
         response = self.client.get(reverse('contact'))
         self.assertTemplateUsed(response, 'blog/contact.html')
+
+
+class SkillsViewTest(TestCase):
+    def test_skills_view_status_code(self):
+        response = self.client.get(reverse('skills'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_skills_view_template(self):
+        response = self.client.get(reverse('skills'))
+        self.assertTemplateUsed(response, 'blog/skills.html')
