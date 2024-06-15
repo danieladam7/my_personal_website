a4c6fc41adbe34307af76f46a6b35443b9ae84f4
Daniel Adam <danielyosef@gmail.com>
2024-06-15 19:12:56 +0300

    implemented Contact View Test

---
+++ blog/tests/test_views.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.core.files.uploadedfile import SimpleUploadedFile
+from ..models import Post, Author, Tag, Comment
+from datetime import date
+
+
+class ContactViewTest(TestCase):
+    def test_contact_view_status_code(self):
+        response = self.client.get(reverse('contact'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_view_template(self):
+        response = self.client.get(reverse('contact'))
+        self.assertTemplateUsed(response, 'blog/contact.html')
