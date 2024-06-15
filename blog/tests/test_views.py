6cddd8d7a4922af9364d813331798f8958d2dbe4
Daniel Adam <danielyosef@gmail.com>
2024-06-15 21:47:02 +0300

    implemented setup of Start Page View Test

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -23,3 +23,23 @@ class SkillsViewTest(TestCase):
     def test_skills_view_template(self):
         response = self.client.get(reverse('skills'))
         self.assertTemplateUsed(response, 'blog/skills.html')
+
+
+class StartPageViewTest(TestCase):
+    def setUp(self):
+        self.author = Author.objects.create(
+            first_name='Israel', last_name='Israeli', email_address='test@pageview.com')
+        self.tag = Tag.objects.create(caption='Test-Tag')
+        self.image = SimpleUploadedFile(
+            name='test_image.jpg', content=b'file_content', content_type='image/jpeg')
+        for i in range(5):
+            post = Post.objects.create(
+                title=f'Test Post {i}',
+                excerpt='Preview of Test Post',
+                image=self.image,
+                date=date.today(),
+                slug=f'test-post-{i}',
+                content='Some test conent.',
+                author=self.author,
+            )
+            post.tags.add(self.tag)
