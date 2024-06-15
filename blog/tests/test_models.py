59163cd4b9d27a13d8016cb2fe62fc073c1db210
Daniel Adam <danielyosef@gmail.com>
2024-06-15 17:01:40 +0300

    not track pycache files and venv

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -7,8 +7,29 @@ class TagModelTest(TestCase):
     def setUp(self):
         self.tag = Tag.objects.create(caption='Test-tag')
 
-    def test_tag_creation(self):
+    def test_tag_caption(self):
         self.assertEqual(self.tag.caption, "Test-tag")
 
     def test_tag_str(self):
         self.assertEqual(str(self.tag), "Test-tag")
+
+
+class AuthorModelTest(TestCase):
+    def setUp(self):
+        self.author = Author.objects.create(
+            first_name='Israel', last_name='Israeli', email_address='test@test.com')
+
+    def test_author_first_name(self):
+        self.assertEqual(self.author.first_name, 'Israel')
+
+    def test_author_last_name(self):
+        self.assertEqual(self.author.last_name, 'Israeli')
+
+    def test_author_email_address(self):
+        self.assertEqual(self.author.email_address, 'test@test.com')
+
+    def test_author_full_name(self):
+        self.assertEqual(self.author.full_name, 'Israel Israeli')
+
+    def test_author_str(self):
+        self.assertEqual(str(self.author), 'Israel Israeli')
