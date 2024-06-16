f76164cf22bca61b653f7e576a637b14c6301be6
Daniel Adam <danielyosef@gmail.com>
2024-06-16 16:44:37 +0300

    implemented  test_single_post_view_context_post_tags

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -151,3 +151,8 @@ class SinglePostViewTest(TestCase):
         response = self.client.get(
             reverse('post-detail-page', args=['test-post']))
         self.assertEqual(response.context['post'], self.post)
+
+
+    def test_single_post_view_context_post_tags(self):
+        response = self.client.get(reverse('post-detail-page', args=['test-post']))
+        self.assertIn('post_tags', response.context)
