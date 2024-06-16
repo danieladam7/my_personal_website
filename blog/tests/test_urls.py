e0537dea48b79e831fd8f24051ac05393ee9a538
Daniel Adam <danielyosef@gmail.com>
2024-06-16 17:41:56 +0300

    implemented test_skills_url_is_resolved

--- blog/tests/test_urls.py
+++ blog/tests/test_urls.py
@@ -3,7 +3,11 @@ from django.urls import reverse, resolve
 from blog.views import StartPageView, AllPostsView, SinglePostView, ContactView, SkillsView
 
 
-class TestUrls(SimpleTestCase):
+class UrlsTest(SimpleTestCase):
     def test_home_url_is_resolved(self):
         url = reverse('home')
         self.assertEqual(resolve(url).func.view_class, StartPageView)
+
+    def test_skills_url_is_resolved(self):
+        url = reverse('skills')
+        self.assertEqual(resolve(url).func.view_class, SkillsView)
